3a2e2be0e75af032d63e54b3e14120606b80e827
nahid96 <nahid.nawaz96@gmail.com>
2019-05-27 22:31:20 +0600

    assignment 1 - PersonalApp

---
+++ Assignment1/PersonalApp/PersonalApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".......Personal Information.......\n");
+
+            Console.WriteLine("Please enter your name:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Please enter your address:");
+            var address = Console.ReadLine();
+
+            Console.WriteLine("Please enter your mobile number:");
+            var mobile = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter your date of birth:");
+            var dateOfBirth = Console.ReadLine();
+
+            Console.WriteLine("Please enter your age:");
+            var age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nName: " + name);
+            Console.WriteLine("Address: " + address);
+            Console.WriteLine("Mobile: 0" + mobile);
+            Console.WriteLine("Date of Birth: " + dateOfBirth);
+            Console.WriteLine("Age: " + age);
+
+        }
+    }
+}
