58047b232d3ce045c64f48849d3aa3b7893068ea
nahid96 <nahid.nawaz96@gmail.com>
2019-05-27 22:32:56 +0600

    assignment 2 - BookShopWinApp using variable

---
+++ Assignment2/BookShopWinApp/BookShopWinApp/BookShop.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookShopWinApp
+{
+    public partial class BookShop : Form
+    {
+        public BookShop()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            string customerName = customerNameTextBox.Text;
+            int contactNo = Convert.ToInt32(contactNoTextBox.Text);
+            string address = addressTextBox.Text;
+            string order = orderComboBox.Text;
+            int quantity = Convert.ToInt32(quantityTextBox.Text);
+
+            customerNameTextBox.Text = string.Empty;
+            contactNoTextBox.Text = string.Empty;
+            addressTextBox.Text = string.Empty;
+            orderComboBox.Text = string.Empty;
+            quantityTextBox.Text = string.Empty;
+
+            detailsRichTextBox.Text = "\tOrder Information" +
+                                       "\t---------------------------"+
+                                      "\n\nCustomer Name : " + customerName +
+                                      "\nContact No. " + contactNo +
+                                        "\nAddress : " + address +
+                                        "\nOrder : " + order +
+                                        "\nQuantity : " + quantity;
+        }
+    }
+}
