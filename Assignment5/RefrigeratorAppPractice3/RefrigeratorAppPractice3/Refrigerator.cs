7f9ada448b1da767aa231a2d714192f70aadb071
nahid96 <nahid.nawaz96@gmail.com>
2019-06-30 00:02:30 +0600

    assignment 5 - RefrigeratorWinApp using OOP(Abstraction, Encapsulation, Data hiding) and using Property, Auto property

---
+++ Assignment5/RefrigeratorAppPractice3/RefrigeratorAppPractice3/Refrigerator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+    class Refrigerator
+    {
+        public int MaxWeight { get; set; }
+        public int CurrentWeight { get; set; }
+
+        bool flag = false;
+
+        public int SetAddWeight
+        {
+            set
+            {
+                AddWeight(value);
+            }
+            get
+            {
+                return Convert.ToInt32(flag);
+            }
+
+        }
+
+        public int RemainingWeight
+        {
+            get {return GetRemainingWeight(); }
+        }
+
+        private bool AddWeight(int weight)
+        {
+            int CheckWeight = CurrentWeight + weight;
+            
+            if (CheckWeight <= MaxWeight)
+            {
+                CurrentWeight += weight;
+                return flag = false;
+            }
+            else
+            {
+                return flag = true;
+            }
+        }
+
+        private int GetRemainingWeight()
+        {
+            int remaingWeight = MaxWeight - CurrentWeight;
+            return remaingWeight;
+        }
+    }
+}
---
+++ Assignment5/RefrigeratorAppPractice3/RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+        private Refrigerator _refrigerator;
+
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+
+            _refrigerator = new Refrigerator();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            _refrigerator.MaxWeight = Convert.ToInt32(maxWeightTakeTextBox.Text);
+
+            maxWeightTakeTextBox.Text = string.Empty;
+            
+            maxWeightTakeTextBox.ReadOnly = true;
+
+            MessageBox.Show("Saved!");
+
+            currentWeightTextBox.Text = _refrigerator.CurrentWeight.ToString();
+            remainingWeightTextBox.Text = _refrigerator.RemainingWeight.ToString();
+
+
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            int weight = (Convert.ToInt32(itemTextBox.Text) * Convert.ToInt32(weightTextBox.Text));
+            
+            _refrigerator.SetAddWeight = weight;
+
+            bool flag = Convert.ToBoolean(_refrigerator.SetAddWeight);
+
+            if (flag == true)
+            {
+                MessageBox.Show("Your given amount can’t be entered because it will be over flown.");
+            }
+
+            currentWeightTextBox.Text = _refrigerator.CurrentWeight.ToString();
+            remainingWeightTextBox.Text = _refrigerator.RemainingWeight.ToString();
+        }
+    }
+}
